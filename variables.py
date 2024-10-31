2a9a89585ce7c4d4033b2c567b96af597f9bf913
C4m1l0Ls <ac.pardo92@gmail.com>
2024-10-30 19:52:28 -0500

    variables python

---
+++ variables.py
@@ -0,0 +1,4 @@
+nombre = "juan" # Variable de tipo Str (cadena de carectres)
+edad = 25 # Variable de tipo Int (Entero)
+altura = 1.72 #Variable de tipo Float (decimales)
+es_estudiante = False # Variable de tipo Bool (booleano)
